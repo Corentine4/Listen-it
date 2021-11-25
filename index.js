d07f74dc71c3e3783fa41cc3df1eb76fb49780a7
Naim <naimzian.dev@gmail.com>
2021-11-25 15:38:04 +0100

    first api calls

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let allMusicArr = [];
+let tableauFin = [];
+const musicList = document.querySelector(".music-list");
+
 function fetchMusic() {
   fetch("https://polar-earth-15538.herokuapp.com/Music")
     .then((response) => response.json())
@@ -11,8 +15,49 @@ fetchMusic();
 
 function fetchMusicComplet(music) {
   let objMusicFull = {};
-  let url = music.url;
-  console.log(music.url);
-  let nameMusic = music.name;
+  //   let url = music.url;
+  //   console.log(music.url);
+  //   let nameMusic = music.name;
   //   console.log(music.name);
+  objMusicFull.id = music.id;
+  objMusicFull.name = music.name;
+  objMusicFull.pic = music.thumbnail;
+  objMusicFull.author = music.author;
+  //   console.log(music.author);
+
+  allMusicArr.push(objMusicFull);
+  if (allMusicArr.length == 2) {
+    console.log(allMusicArr);
+    tableauFin = allMusicArr
+      .sort((a, b) => {
+        return a.id - b.id;
+      })
+      .slice(0, 2);
+    // console.log(tableauFin);
+    createCard(tableauFin);
+  }
+}
+//Ajout du pokedex
+function createCard(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    const carte = document.createElement("li");
+    const txtCarteNom = document.createElement("h5");
+    txtCarteNom.innerText = arr[i].name;
+    const txtCarteAuteur = document.createElement("h5");
+    txtCarteAuteur.innerText = arr[i].author;
+    const idCarte = document.createElement("p");
+    idCarte.innerText = `id: ${arr[i].id}`;
+    const imgCarte = document.createElement("img");
+    imgCarte.src = arr[i].pic;
+
+    // ajoute enfants à li
+    carte.appendChild(imgCarte);
+    carte.appendChild(txtCarteNom);
+    carte.appendChild(txtCarteAuteur);
+    carte.appendChild(idCarte);
+
+    //Ajout de carte dans ul.listePoke
+    musicList.appendChild(carte);
+  }
 }

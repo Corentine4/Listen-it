063a3cbd3b3e92313390de54ed522ae5af3b7dcd
Corentine <renuart.corentine@gmail.com>
2021-11-29 11:29:39 +0100

    Create pages

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// /!\ "Routes" and not "Switch"
+
+function App() {
+  return (
+    <div className="App">
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NotFound/>}/>
+          <Route path="/" exact element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;

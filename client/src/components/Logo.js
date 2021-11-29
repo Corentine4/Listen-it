c3d5d84410e5b27b38cad1581017a00b32ca71f7
Corentine <renuart.corentine@gmail.com>
2021-11-29 12:32:05 +0100

    Adding music logo

---
+++ client/src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div>
+            <img src="././Images/music-solid.svg" width="50rem" alt="Note de musique"/>
+        </div>
+    );
+};
+
+export default Logo;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 
+// Import the logo
+import Logo from '../components/Logo';
+
 const Home = () => {
     return (
         <div className="home">
+            <Logo/>
             <Navigation/>
             <h1>Accueil</h1>
         </div>
